ed170ac3eafb31100aab93eb55834f30365f5823
vidheshashetty <vidheshashetty@gmail.com>
2026-04-08 01:43:54 +0530

    Update pyspark_job.py

--- glue_jobs/pyspark_job.py
+++ glue_jobs/pyspark_job.py
@@ -5,9 +5,7 @@ from pyspark.sql.window import Window
 spark = SparkSession.builder.appName("BankingPipeline").getOrCreate()
 
 # Read raw data
-df = spark.read.format("csv") \
-    .option("header", True) \
-    .load("s3://raw-bucket/transactions/")
+df = spark.read.csv("data/raw/", header=True, inferSchema=True)
 
 # Deduplication
 window_spec = Window.partitionBy("trans_id").orderBy(col("trans_timestamp").desc())
@@ -32,5 +30,5 @@ df_failed = df_dedup.filter(
 )
 
 
-df_clean.write.mode("overwrite").parquet("s3://processed-bucket/clean/")
-df_failed.write.mode("overwrite").parquet("s3://processed-bucket/failed/")
+df_clean.write.mode("overwrite").parquet("data/processed/clean/")
+df_failed.write.mode("overwrite").parquet("data/processed/failed/")
